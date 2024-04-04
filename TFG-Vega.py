fbb713ead4acd4f300f19d480d0a8e61cd1aaa41
Ainhoa <ainhoa.urtasun@unavarra.es>
2024-04-05 01:02:06 +0200

    Update TFG-Vega.py

--- TFG-Vega.py
+++ TFG-Vega.py
@@ -33,13 +33,7 @@ world = geopandas.read_file('/content/TFG-Vega/ne_110m_admin_0_countries.zip')[[
 polygon = Polygon([(-25,35),(40,35),(40,75),(-25,75)])
 europe = geopandas.clip(world,polygon)
 
-mydata1 = mydata[mydata.time=='2021']
-table = mydata.pivot(index='ADMIN',columns='time',values=['percentage']).reset_index()
-table.columns = table.columns.droplevel(level=0)
-table.rename(columns={'ADMIN':'GEO'},inplace=True)
-table = table.rename_axis(columns=None)
-
-mydata1 = mydata1.merge(europe,on='ADMIN',how='right')
+mydata = mydata.merge(europe,on='ADMIN',how='right')
 mydata1 = geopandas.GeoDataFrame(mydata1,geometry='geometry')
 fig,ax = plt.subplots(1,figsize=(10,10))
 mydata1.plot(column='percentage',alpha=0.8,cmap='viridis',ax=ax,legend=True)
