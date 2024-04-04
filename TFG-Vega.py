8b630a4f96099a144ea4c5d42dff6ce378ae8cab
Ainhoa <ainhoa.urtasun@unavarra.es>
2024-04-05 00:54:56 +0200

    Update TFG-Vega.py

--- TFG-Vega.py
+++ TFG-Vega.py
@@ -16,7 +16,7 @@ data = pandas.Series(metadata['value']).rename(index=int).sort_index()
 n = 1 # Initialize the result to 1
 for num in metadata['size']:
   n *= num
-data = data.reindex(range(0,n))
+data = data.reindex(range(0,n),fill_value=0)
 structure = [pandas.DataFrame({key:val for key,val in metadata['dimension'][dim]['category'].items()}).sort_values('index')['label'].values for dim in metadata['id']]
 data.index = pandas.MultiIndex.from_product(structure,names=metadata['id'])
 mydata = data.reset_index()
