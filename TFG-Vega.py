0c25942af9caa653d145f660a8a4761ba836e2c6
Ainhoa <ainhoa.urtasun@unavarra.es>
2024-04-05 07:58:03 +0200

    Update TFG-Vega.py

--- TFG-Vega.py
+++ TFG-Vega.py
@@ -49,7 +49,6 @@ data = data.reindex(range(0,n),fill_value=0)
 structure = [pandas.DataFrame({key:val for key,val in metadata['dimension'][dim]['category'].items()}).sort_values('index')['label'].values for dim in metadata['id']]
 data.index = pandas.MultiIndex.from_product(structure,names=metadata['id'])
 mydata2 = data.reset_index()
-mydata2['lev_satis'] == 'High'
 mydata2['isced11'] == 'All ISCED 2011 levels'
 mydata2['indic_wb'] == 'Overall life satisfaction'
 mydata2['sex'] == 'Total'
